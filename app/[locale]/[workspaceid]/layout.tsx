c0b24246bcc0ed4fbe095c9fdf38cb602978de8d
armanengin <a.enginsucu@gmail.com>
2024-03-06 12:28:17 -0800

    init commit

---
+++ app/[locale]/[workspaceid]/layout.tsx
@@ -0,0 +1,183 @@
+"use client"
+
+import { Dashboard } from "@/components/ui/dashboard"
+import { ChatbotUIContext } from "@/context/context"
+import { getAssistantWorkspacesByWorkspaceId } from "@/db/assistants"
+import { getChatsByWorkspaceId } from "@/db/chats"
+import { getCollectionWorkspacesByWorkspaceId } from "@/db/collections"
+import { getFileWorkspacesByWorkspaceId } from "@/db/files"
+import { getFoldersByWorkspaceId } from "@/db/folders"
+import { getModelWorkspacesByWorkspaceId } from "@/db/models"
+import { getPresetWorkspacesByWorkspaceId } from "@/db/presets"
+import { getPromptWorkspacesByWorkspaceId } from "@/db/prompts"
+import { getAssistantImageFromStorage } from "@/db/storage/assistant-images"
+import { getToolWorkspacesByWorkspaceId } from "@/db/tools"
+import { getWorkspaceById } from "@/db/workspaces"
+import { convertBlobToBase64 } from "@/lib/blob-to-b64"
+import { supabase } from "@/lib/supabase/browser-client"
+import { LLMID } from "@/types"
+import { useParams, useRouter } from "next/navigation"
+import { ReactNode, useContext, useEffect, useState } from "react"
+import Loading from "../loading"
+
+interface WorkspaceLayoutProps {
+  children: ReactNode
+}
+
+const prompt =
+  'As a Cognitive Behavioral Therapist, your kind and open approach to CBT allows users to confide in you. You ask questions one by one and collect the user\'s responses to implement the following steps of CBT: Help the user identify troubling situations or conditions in their life. Help the user become aware of their thoughts, emotions, and beliefs about these problems. Using the user\'s answers to the questions, you identify and categorize negative or inaccurate thinking that is causing the user anguish into one or more of the following CBT-defined categories: All-or-Nothing Thinking, Overgeneralization, Mental Filter, Disqualifying the Positive, Jumping to Conclusions, Mind Reading, Fortune Telling, Magnification (Catastrophizing) or Minimization, Emotional Reasoning, Should Statements, Labeling and Mislabeling, Personalization. After identifying and informing the user of the type of negative or inaccurate thinking based on the above list, you help the user reframe their thoughts through cognitive restructuring. You ask questions one at a time to help the user process each question separately. For example, you may ask: What evidence do I have to support this thought? What evidence contradicts it? Is there an alternative explanation or perspective for this situation? Am I overgeneralizing or applying an isolated incident to a broader context? Am I engaging in black-and-white thinking or considering the nuances of the situation? Am I catastrophizing or exaggerating the negative aspects of the situation? Am I taking this situation personally or blaming myself unnecessarily? Am I jumping to conclusions or making assumptions without sufficient evidence? Am I using "should" or "must" statements that set unrealistic expectations for myself or others? Am I engaging in emotional reasoning, assuming that my feelings represent the reality of the situation? Am I using a mental filter that focuses solely on the negative aspects while ignoring the positives? Am I engaging in mind reading, assuming I know what others are thinking or feeling without confirmation? Am I labeling myself or others based on a single event or characteristic? How would I advise a friend in a similar situation? What are the potential consequences of maintaining this thought? How would changing this thought benefit me? Is this thought helping me achieve my goals or hindering my progress? Using the user\'s answers, you ask them to reframe their negative thoughts with your expert advice. As a parting message, you can reiterate and reassure the user with a hopeful message.'
+
+export default function WorkspaceLayout({ children }: WorkspaceLayoutProps) {
+  const router = useRouter()
+
+  const params = useParams()
+  const workspaceId = params.workspaceid as string
+
+  const {
+    setChatSettings,
+    setAssistants,
+    setAssistantImages,
+    setChats,
+    setCollections,
+    setFolders,
+    setFiles,
+    setPresets,
+    setPrompts,
+    setTools,
+    setModels,
+    selectedWorkspace,
+    setSelectedWorkspace,
+    setSelectedChat,
+    setChatMessages,
+    setUserInput,
+    setIsGenerating,
+    setFirstTokenReceived,
+    setChatFiles,
+    setChatImages,
+    setNewMessageFiles,
+    setNewMessageImages,
+    setShowFilesDisplay
+  } = useContext(ChatbotUIContext)
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    ;(async () => {
+      const session = (await supabase.auth.getSession()).data.session
+
+      if (!session) {
+        return router.push("/login")
+      } else {
+        await fetchWorkspaceData(workspaceId)
+      }
+    })()
+  }, [])
+
+  useEffect(() => {
+    ;(async () => await fetchWorkspaceData(workspaceId))()
+
+    setUserInput("")
+    setChatMessages([])
+    setSelectedChat(null)
+
+    setIsGenerating(false)
+    setFirstTokenReceived(false)
+
+    setChatFiles([])
+    setChatImages([])
+    setNewMessageFiles([])
+    setNewMessageImages([])
+    setShowFilesDisplay(false)
+  }, [workspaceId])
+
+  const fetchWorkspaceData = async (workspaceId: string) => {
+    setLoading(true)
+
+    const workspace = await getWorkspaceById(workspaceId)
+    setSelectedWorkspace(workspace)
+
+    const assistantData = await getAssistantWorkspacesByWorkspaceId(workspaceId)
+    setAssistants(assistantData.assistants)
+
+    for (const assistant of assistantData.assistants) {
+      let url = ""
+
+      if (assistant.image_path) {
+        url = (await getAssistantImageFromStorage(assistant.image_path)) || ""
+      }
+
+      if (url) {
+        const response = await fetch(url)
+        const blob = await response.blob()
+        const base64 = await convertBlobToBase64(blob)
+
+        setAssistantImages(prev => [
+          ...prev,
+          {
+            assistantId: assistant.id,
+            path: assistant.image_path,
+            base64,
+            url
+          }
+        ])
+      } else {
+        setAssistantImages(prev => [
+          ...prev,
+          {
+            assistantId: assistant.id,
+            path: assistant.image_path,
+            base64: "",
+            url
+          }
+        ])
+      }
+    }
+
+    const chats = await getChatsByWorkspaceId(workspaceId)
+    setChats(chats)
+
+    const collectionData =
+      await getCollectionWorkspacesByWorkspaceId(workspaceId)
+    setCollections(collectionData.collections)
+
+    const folders = await getFoldersByWorkspaceId(workspaceId)
+    setFolders(folders)
+
+    const fileData = await getFileWorkspacesByWorkspaceId(workspaceId)
+    setFiles(fileData.files)
+
+    const presetData = await getPresetWorkspacesByWorkspaceId(workspaceId)
+    setPresets(presetData.presets)
+
+    const promptData = await getPromptWorkspacesByWorkspaceId(workspaceId)
+    setPrompts(promptData.prompts)
+
+    const toolData = await getToolWorkspacesByWorkspaceId(workspaceId)
+    setTools(toolData.tools)
+
+    const modelData = await getModelWorkspacesByWorkspaceId(workspaceId)
+    setModels(modelData.models)
+
+    setChatSettings({
+      model: (workspace?.default_model || "gpt-4-1106-preview") as LLMID,
+      prompt: prompt,
+      // workspace?.default_prompt ||
+      // "You are a friendly, helpful AI assistant.",
+      temperature: workspace?.default_temperature || 0.5,
+      contextLength: workspace?.default_context_length || 4096,
+      includeProfileContext: workspace?.include_profile_context || true,
+      includeWorkspaceInstructions:
+        workspace?.include_workspace_instructions || true,
+      embeddingsProvider:
+        (workspace?.embeddings_provider as "openai" | "local") || "openai"
+    })
+
+    setLoading(false)
+  }
+
+  if (loading) {
+    return <Loading />
+  }
+
+  return <Dashboard>{children}</Dashboard>
+}
